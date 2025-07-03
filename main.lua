10abb318a572355f80117d4a6e51e6d526aba9f8
Stellar <124856838+Repcoders@users.noreply.github.com>
2025-07-03 12:15:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
 
+local Window = Library:CreateWindow({
+    Title = "Infinite Realm - Ink Game",
+    Footer = "v1.0.0 - BETA",
+    ToggleKeybind = Enum.KeyCode.RightControl,
+    Center = true,
+    AutoShow = true
+})
