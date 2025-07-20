fec4f486d5cbe19c5ba0960af8d327253b3e2444
NetlessScripter <iamtheatm1@gmail.com>
2025-07-19 23:39:29 -0400

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -129,37 +129,20 @@ function UILib:NewTab(tabName, iconId)
 
 		local SectionObject = {}
 
-		function SectionObject:NewButton(text, desc, callback, iconId)
+		function SectionObject:NewButton(text, desc, callback)
 			local button = Instance.new("ImageButton", tabPage)
 			button.Size = UDim2.new(0, 300, 0, 45)
-			button.BackgroundTransparency = 0.3
-			button.BackgroundColor3 = Color3.fromRGB(33, 132, 66)
-			button.AutoButtonColor = true
+			button.BackgroundTransparency = 1
 			button.Image = "rbxassetid://6803353442"
 			button.Name = text
-			button.ScaleType = Enum.ScaleType.Slice
-			button.SliceCenter = Rect.new(10, 10, 118, 118)
-			button.SliceScale = 0.1
-
-			if iconId then
-				local icon = Instance.new("ImageLabel", button)
-				icon.Size = UDim2.new(0, 28, 0, 28)
-				icon.Position = UDim2.new(0, 7, 0.5, -14)
-				icon.Image = iconId
-				icon.BackgroundTransparency = 1
-				icon.Name = "Icon"
-			end
 
 			local label = Instance.new("TextLabel", button)
 			label.Text = text .. (desc and (" - " .. desc) or "")
 			label.Font = Enum.Font.RobotoMono
 			label.TextSize = 18
 			label.TextColor3 = Color3.new(1, 1, 1)
+			label.Size = UDim2.new(1, 0, 1, 0)
 			label.BackgroundTransparency = 1
-			label.TextXAlignment = Enum.TextXAlignment.Left
-			label.Size = UDim2.new(1, iconId and -42 or -14, 1, 0)
-			label.Position = UDim2.new(iconId and 0, 42 or 0, 0, 0)
-			label.Name = "Label"
 
 			button.MouseButton1Click:Connect(callback)
 		end
